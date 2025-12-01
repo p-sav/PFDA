7efbbf1f7d01709ac2972e8e371a662a1c37cf4b
p-sav <info@savelectrical.com>
2025-12-01 22:59:08 +0000

    1st Dec

---
+++ My-Work/Week-7/regression.py
@@ -0,0 +1,23 @@
+# regression.py
+# This script loads the 'tips' dataset from the seaborn library
+# and creates a scatter plot with a linear regression line
+# using seaborn and matplotlib libraries.
+
+
+
+# import the library
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load the dataset
+dataset = sns.load_dataset('tips')
+
+# the first five entries of the dataset
+print(dataset.head())
+sns.set_style('whitegrid')
+# sns.scatterplot(x='total_bill', y='tip', data=dataset)
+sns.lmplot(x='total_bill', y='tip', order=1, data=dataset)
+
+plt.show()
+
+
